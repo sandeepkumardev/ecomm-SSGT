5108ddcf89c8e7e8ee5334de7c4f7ff836c6bb9d
sandeep kumar <sandeepypb@gmail.com>
2025-08-05 12:20:43 +0530

    ✨ Feat: list users

--- admin/src/pages/Users.jsx
+++ admin/src/pages/Users.jsx
@@ -1,8 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Layout from "../components/Layout";
 import withAuth from "../components/withAuth";
 
 const Users = () => {
+  const [admins, setAdmins] = useState([]);
+  const [users, setUsers] = useState([]);
+
   useEffect(() => {
     const getData = async () => {
       try {
@@ -21,14 +24,88 @@ const Users = () => {
           return;
         }
 
-        console.log(data.data);
+        setAdmins(data.data.filter((user) => user.role === "admin"));
+        setUsers(data.data.filter((user) => user.role === "user"));
       } catch (error) {
         console.log(error);
       }
     };
     getData();
   }, []);
-  return <Layout>Users page</Layout>;
+
+  return (
+    <Layout>
+      <div className="flex justify-between items-center p-3 border-b border-gray-300 h-[50px] select-none">
+        <h1 className="font-bold">Admins</h1>
+      </div>
+
+      {/* admins list table */}
+      <div className="overflow-x-auto">
+        <table className="min-w-full divide-y divide-gray-200">
+          <thead className="bg-gray-50">
+            <tr>
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Name
+              </th>
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Email
+              </th>
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Role
+              </th>
+              {/* <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Actions
+              </th> */}
+            </tr>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
+            {admins.map((user) => (
+              <tr key={user._id} className="border border-x-0 border-gray-300">
+                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{user.name}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{user.email}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{user.role}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <div className="mt-10 flex justify-between items-center p-3 border-b border-gray-300 h-[50px] select-none">
+        <h1 className="font-bold">Users</h1>
+      </div>
+
+      {/* users list table */}
+      <div className="overflow-x-auto">
+        <table className="min-w-full divide-y divide-gray-200">
+          <thead className="bg-gray-50">
+            <tr>
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Name
+              </th>
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Email
+              </th>
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Role
+              </th>
+              {/* <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Actions
+              </th> */}
+            </tr>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
+            {users.map((user) => (
+              <tr key={user._id} className="border border-x-0 border-gray-300">
+                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{user.name}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{user.email}</td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{user.role}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </Layout>
+  );
 };
 
 export default withAuth(Users);
